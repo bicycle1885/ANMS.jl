377e0f0feb9c5bc06eff5741599f286381b58a6f
Kenta Sato <bicycle1885@gmail.com>
2015-03-03 19:04:04 +0900

    add benchmark

---
+++ benchmark/runbenchmarks.jl
@@ -0,0 +1,14 @@
+using ANMS
+
+quadratic(x) = dot(x, x)
+
+let
+    x₀ = ones(2)
+    nelder_mead(quadratic, x₀)
+
+    for n in [2, 5, 10, 50, 100, 500]
+        x₀ = ones(n)
+        @time x, fmin = nelder_mead(quadratic, x₀)
+        @assert fmin < 1.0e-5
+    end
+end
